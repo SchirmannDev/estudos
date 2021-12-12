c0a84e002a3b30c3c7bd1380f13ae076a179f0b9
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-12 19:34:04 -0300

    Object assign

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("olá mundo");
+const javaScript = Object.create({});
+Object.assign(
+  javaScript,
+  {
+    name: "JavaScrip",
+    year: 1995,
+    paradigm: "00 and Functional",
+  },
+  {
+    author: "Brendan Eich",
+    influenceBy: "Java, Scheme and Self",
+  }
+);
+
+console.log(javaScript);
+
+//assing serve para atribuir mais descrições ao objeto principal
