d31d9b0a5235c7150eeddaeeebc355afe639dc30
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-13 17:56:39 -0300

    utilizando new Date e object.setproto

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const person1 = {
-  name: "Linus Torvalds",
-  city: "Helsinki",
-  year: 1969,
+const personPrototype = {
   getAge() {
     return new Date().getFullYear() - this.year;
   },
+};
+const createPerson = function (name, city, year) {
+  const person = {
+    name,
+    city,
+    year,
+  };
+  Object.setPrototypeOf(person, personPrototype);
+  return person;
 };
 
-const person2 = {
-  name: "Bill Gates",
-  city: "Seattle",
-  year: 1955,
-  getAge() {
-    return new Date().getFullYear() - this.year;
-  },
-};
+const person1 = createPerson("Linus Torvalds", "Helsinki", 1969);
+const person2 = createPerson("Bill Gates", "Seattle", 1955);
 
 console.log(person1);
 console.log(person1.getAge());
-
 console.log(person2);
 console.log(person2.getAge());
