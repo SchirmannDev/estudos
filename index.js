730b2314e6bfe634ec643a30bbb6b4a45dc62a5e
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-13 18:47:38 -0300

    utilizando new e apply

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const personPrototype = {
-  getAge() {
-    return new Date().getFullYear() - this.year;
-  },
-};
-const createPerson = function (name, city, year) {
-  const person = {
-    name,
-    city,
-    year,
-  };
-  Object.setPrototypeOf(person, personPrototype);
-  return person;
+const _new = function (fn, ...params) {
+  const obj = {};
+  Object.setPrototypeOf(obj, fn.prototype);
+  fn.apply(obj, params);
+  return obj;
 };
 
-const person1 = createPerson("Linus Torvalds", "Helsinki", 1969);
-const person2 = createPerson("Bill Gates", "Seattle", 1955);
+const Person = function (name, city, year) {
+  this.name = name;
+  this.city = city;
+  this.year = year;
+};
+
+Person.prototype.getAge = function () {
+  return new Date().getFullYear() - this.year;
+};
+
+const person1 = _new(Person, "Linus Torvalds", "Helsinki", 1969);
+const person2 = _new(Person, "Bill Gates", "Seattle", 1955);
 
 console.log(person1);
+console.log(person1.__proto__);
 console.log(person1.getAge());
 console.log(person2);
+console.log(person2.__proto__);
 console.log(person2.getAge());
+
+console.log(person1.__proto__ === person2.__proto__);
