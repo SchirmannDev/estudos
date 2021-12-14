5ab5fb31db9a8c85bb05829e626ae1df3ebc4139
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-13 23:17:56 -0300

    utilizando new e instanceof

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-const _new = function (fn, ...params) {
-  const obj = {};
-  Object.setPrototypeOf(obj, fn.prototype);
-  fn.apply(obj, params);
-  return obj;
+const _instanceof = function (obj, fn) {
+  if (obj === fn.prototype) return true;
+  if (obj === null) return false;
+  return _instanceof(obj.__proto__, fn);
 };
 
-const Person = function (name, city, year) {
-  this.name = name;
-  this.city = city;
-  this.year = year;
-};
+const date = new Date();
+console.log(date instanceof Date);
+console.log(date instanceof Object);
+console.log(date instanceof Array);
 
-Person.prototype.getAge = function () {
-  return new Date().getFullYear() - this.year;
-};
-
-const person1 = _new(Person, "Linus Torvalds", "Helsinki", 1969);
-const person2 = _new(Person, "Bill Gates", "Seattle", 1955);
-
-console.log(person1);
-console.log(person1.__proto__);
-console.log(person1.getAge());
-console.log(person2);
-console.log(person2.__proto__);
-console.log(person2.getAge());
-
-console.log(person1.__proto__ === person2.__proto__);
+console.log(_instanceof(date, Date));
+console.log(_instanceof(date, Object));
+console.log(_instanceof(date, Array));
